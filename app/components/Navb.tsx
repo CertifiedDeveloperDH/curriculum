418c9fc70c601c0add906f9ed58a83d0941f23d7
certifieddeveloper085 <certifieddeveloper085@gmail.com>
2024-05-08 11:14:17 -0300

    curriculum

--- app/components/Navb.tsx
+++ app/components/Navb.tsx
@@ -6,63 +6,49 @@ import { usePathname } from "next/navigation";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Container from 'react-bootstrap/Container'
-import './YourNavbar.css'; // Import your custom CSS file
-
-//import styles from "../styles/layout.module.css";
+import styles from "../styles/layout.module.css";
 
 export const Navb = () => {
-  const pathname = usePathname();
-
-  const [selectedLink, setSelectedLink] = useState('home');
-
-  const handleSelect = (selectedKey:string|null) => {
-    if (selectedKey !== null){
-      setSelectedLink(selectedKey);
-    }
-  };
-
-  const navbarStyle = {
-    backgroundImage: 'url("/code.jpg")',
-    backgroundSize: 'cover', // Ensure the image covers the entire Navbar
-    position: 'relative', // Ensure that the Navbar content respects z-index
-    minHeight: "100vh",
-    alignItems: "start",
-  };
-  const toggleStyle = {
-    zIndex: '2', // Set a higher z-index for Navbar toggle
-  };
+  const pathname = usePathname()
 
   return (
     <Navbar collapseOnSelect expand='lg'>
       <img src="/code-brackets.svg" className="me-auto" style={{maxHeight:"3em", maxWidth:"3em"}}/>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" className= "ms-auto" style={toggleStyle}/>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" className= "ms-auto"/>
       <Navbar.Collapse id="responsive-navbar-nav">
         <p className="me-auto">Logo</p>
-        <Nav className="ms-auto" 
-        activeKey={selectedLink}
-        onSelect={handleSelect}
-        >
-          <Nav.Link style={{marginRight:"3vw",color:"black"}} eventKey = "home" className={selectedLink === "home" ? "selected-link" : ""}
-            href="/"
-          >
-            Home
-          </Nav.Link>
-          <Nav.Link style={{marginRight:"3vw", color:"black"}} eventKey="about" className={selectedLink === "about" ? "selected-link": ""}
-            href="/aboutme"
-          >
-            Aboutme
-          </Nav.Link>
-          <Nav.Link style={{marginRight:"3vw", color:"black"}} eventKey = "verify" className={selectedLink==="verify" ? "selected-link": ""}
-            href="/verify"
-          >
-            Verify
-          </Nav.Link>
-          <Nav.Link style={{color:"black"}} eventKey = "quotes" className={selectedLink==="quotes" ? "selected-link": ""}
-            href="/quotes"
-            >
-            Quotes
-          </Nav.Link>
+        <Nav className="ms-auto">
+        <Link
+        className={`${styles.link} ${pathname === "/" ? styles.active : " "}`}
+        href="/"
+      >
+        Home
+      </Link>
+      <Link
+        className={`${styles.link} ${
+          pathname === "/aboutme" ? styles.active : " "
+
+        }`}
+        href="/aboutme"
+      >
+        Aboutme
+      </Link>
+      <Link
+        className={`${styles.link} ${
+          pathname === "/verify" ? styles.active : " "
+        }`}
+        href="/verify"
+      >
+        Verify
+      </Link>
+      <Link
+        className={`${styles.link} ${
+          pathname === "/quotes" ? styles.active : " "
+        }`}
+        href="/quotes"
+      >
+        Quotes
+      </Link>
           </Nav>
       </Navbar.Collapse>
     </Navbar>
